4503200b6089aabaf6135e710e31878842fe9612
Aleksundraaa <ladycaxap@gmail.com>
2025-10-19 01:15:03 +0500

    Тг бот черновая версия
    # Conflicts:
    #	BlazorApp1/obj/BlazorApp1.csproj.nuget.dgspec.json
    #	BlazorApp1/obj/BlazorApp1.csproj.nuget.g.targets
    #	BlazorApp1/obj/Debug/net6.0/BlazorApp1.assets.cache
    #	BlazorApp1/obj/Debug/net6.0/BlazorApp1.csproj.AssemblyReference.cache
    #	BlazorApp1/obj/project.assets.json
    #	BlazorApp1/obj/project.nuget.cache
    #	BlazorApp1/obj/project.packagespec.json
    #	BlazorApp1/obj/rider.project.model.nuget.info
    #	BlazorApp1/obj/rider.project.restore.info

---
+++ BlazorApp1/Bot/DatabaseService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+using System.IO;
+namespace BlazorApp1.Bot;
+
+public class DatabaseService
+{
+    private readonly string connectionString;
+
+    public DatabaseService()
+    {
+        var dbPath = Path.Combine(AppContext.BaseDirectory, "DataBase.db");
+        connectionString = $"Data Source={dbPath}";
+        Console.WriteLine($"База данных создаётся здесь: {dbPath}");
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        using var connection = new SqliteConnection(connectionString);
+        connection.Open();
+        
+        var command = connection.CreateCommand();
+        command.CommandText = 
+            """
+            CREATE TABLE IF NOT EXISTS Users (
+                TelegramId INTEGER PRIMARY KEY,
+                FullName TEXT NOT NULL,
+                UniqueCode INTEGER NOT NULL 
+            );
+            """;
+        command.ExecuteNonQuery();
+    }
+    
+}
--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,31 +1,74 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazorApp1.Data;
+// using Microsoft.AspNetCore.Components;
+// using Microsoft.AspNetCore.Components.Web;
+// using BlazorApp1.Data;
+//
+// var builder = WebApplication.CreateBuilder(args);
+//
+// // Add services to the container.
+// builder.Services.AddRazorPages();
+// builder.Services.AddServerSideBlazor();
+// builder.Services.AddSingleton<WeatherForecastService>();
+//
+// var app = builder.Build();
+//
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+//
+// app.UseHttpsRedirection();
+//
+// app.UseStaticFiles();
+//
+// app.UseRouting();
+//
+// app.MapBlazorHub();
+// app.MapFallbackToPage("/_Host");
+//
+// app.Run();
 
-var builder = WebApplication.CreateBuilder(args);
+using BlazorApp1.Bot;
+using static BlazorApp1.Bot.UserManager;
+using Microsoft.Data.Sqlite;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    const string token = "8450218559:AAGCQdk6hnrtP8aFZpZM-bCc7tCWeKNWaIE";
+    public static async Task Main()
+    {
+        var db = new DatabaseService();
+        var dbPath = Path.Combine(AppContext.BaseDirectory, "DataBase.db");
+        var userManager = new UserManager(dbPath);
+        
+        var bot = new TelegramBotService(token, userManager);
+
+        await bot.StartAsync(CancellationToken.None);
+
+        Console.WriteLine("Бот запущен. Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+
+        Console.WriteLine(ShowBd());
+    }
+
+    private static string ShowBd()
+    {
+        var list = new List<string>();
+        var dbPath1 = @"C:\Users\_\Desktop\task_tracker_proj\BlazorApp1\bin\Debug\net6.0\DataBase.db";
+        using var connection = new SqliteConnection($"Data Source={dbPath1}");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Users";
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            list.Add($"{reader["TelegramId"]}, {reader["FullName"]}, {reader["UniqueCode"]}");
+        }
+
+        return string.Join("\n", list);
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
