15c039fcf19cf46cf2002bbd47abc032a13e0693
evgeniavolkova <evgeniafil04@gmail.com>
2025-11-30 12:51:29 +0500

    сделала class Entity абстрактным. реализовала все методы

--- SMMTracker.Domain/Entities/Entity.cs
+++ SMMTracker.Domain/Entities/Entity.cs
@@ -1,27 +1,42 @@
 namespace SMMTracker.Domain.Entities;
 
-public class Entity
+public abstract class Entity
 {
-    public int Id { get; private set; }
+    public int Id { get; protected set; }
 
     public override bool Equals(object? obj)
     {
-        throw new NotImplementedException();
-
+        return obj is Entity other && Equals(other);
     }
 
-    protected bool Equals(Entity other)
+    public bool Equals(Entity? other)
     {
-        throw new NotImplementedException();
+        if (other is null) return false;
+        if (Id == 0 || other.Id == 0)
+            return ReferenceEquals(this, other);
+
+        return Id == other.Id;
     }
 
     public override int GetHashCode()
     {
-        throw new NotImplementedException();
+        return Id.GetHashCode();
     }
 
     public override string ToString()
     {
-        throw new NotImplementedException();
+       return $"Name: {GetType().Name} Id: {Id}";
+    }
+
+    public static bool operator ==(Entity? left, Entity? right)
+    {
+        if (left is null)
+            return right is null;
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Entity? left, Entity? right)
+    {
+        return !(left == right);
     }
 }
