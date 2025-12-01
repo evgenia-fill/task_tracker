f708090e978e2182cf61d2272a7d83a3af12c3d1
evgeniavolkova <evgeniafil04@gmail.com>
2025-12-01 23:09:40 +0500

    всё для ивента

--- SMMTracker.Domain/Entities/Event.cs
+++ SMMTracker.Domain/Entities/Event.cs
@@ -6,12 +6,23 @@ namespace SMMTracker.Domain.Entities;
 
 public class Event : Entity
 {
-    public string? Name { get; set; }
-    public EventStatus Status { get; set; }
-    public string? Description { get; set; }
-    public DateTime Date { get; set; }
-    public int CalendarId { get; set; }
+    public string? Name { get; private set; }
+    public string? Description { get; private set; }
+    public DateTime Date { get; private set; }
+    public int CalendarId { get; private set; }
 
-    public Calendar? Calendar { get; set; }
-    public List<Task>? Tasks { get; set; } 
+    public Calendar Calendar { get; private set; } = null!;
+    public List<Task> Tasks { get; private set; } = new();
+
+    public Event(string? name, string? description, DateTime date, int calendarId)
+    {
+        Name = name;
+        Description = description;
+        Date = date;
+        CalendarId = calendarId;
+    }
+
+    private Event()
+    {
+    }
 }
