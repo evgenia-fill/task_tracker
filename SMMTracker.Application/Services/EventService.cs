18e3382e47189996fa1d4aa714d08eb1722d4c6f
evgeniavolkova <evgeniafil04@gmail.com>
2025-12-08 14:45:01 +0500

    все про ивенты и календарь. можно посмотреть детали мероприятия и все мероприятия в данном месяце

--- SMMTracker.Application/Services/EventService.cs
+++ SMMTracker.Application/Services/EventService.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SMMTracker.Domain.Entities;
 using SMMTracker.Application.Dtos;
 using SMMTracker.Application.Interfaces;
@@ -25,5 +26,42 @@ public class EventService
         _context.Events.Add(eventAs);
         await _context.SaveChangesAsync(cancellationToken);
         return eventAs.Id;
+    }
+
+    public async Task<List<EventSummaryDto>> GetEventsForMonthAsync(int calendarId, int month, int year,
+        CancellationToken cancellationToken = default)
+    {
+        return await _context.Events
+            .Where(e => e.CalendarId == calendarId && e.Date.Year == year && e.Date.Month == month)
+            .Select(e => new EventSummaryDto
+            {
+                Id = e.Id,
+                Name = e.Name,
+                Date = e.Date
+            })
+            .ToListAsync(cancellationToken: cancellationToken);
+    }
+
+    public async Task<EventDetailsDto?> GetEventDetailsAsync(int eventId, CancellationToken cancellationToken = default)
+    {
+        return await _context.Events
+            .Where(e => e.Id == eventId)
+            .Include(e => e.Tasks)
+            .Select(e =>
+                new EventDetailsDto
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Description = e.Description,
+                    Date = e.Date,
+                    Tasks = e.Tasks
+                        .Select(t => new TaskSummaryDto
+                        {
+                            Id = t.Id,
+                            Name = t.Name,
+                            Status = (TaskStatus)t.Status
+                        }).ToList()
+                })
+            .FirstOrDefaultAsync(cancellationToken: cancellationToken);
     }
 }
