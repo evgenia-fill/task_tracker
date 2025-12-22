9060493ae4e8845b275b066e831f46b865171851
Aleksundraaa <ladycaxap@gmail.com>
2025-12-23 00:58:57 +0500

    миграции

--- SMMTracker.Infrastructure/Data/Migrations/20251222195229_AddUserIdToCalendar.Designer.cs
+++ SMMTracker.Infrastructure/Data/Migrations/20251222195229_AddUserIdToCalendar.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SMMTracker.Infrastructure.Data.DataContext;
 
@@ -10,9 +11,11 @@ using SMMTracker.Infrastructure.Data.DataContext;
 namespace SMMTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251222195229_AddUserIdToCalendar")]
+    partial class AddUserIdToCalendar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
