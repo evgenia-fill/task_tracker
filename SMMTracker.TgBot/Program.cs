4de09036b4f9e7c776668e335dd46cffd35597af
Mariya <maripolevay@gmail.com>
2025-12-23 22:04:30 +0500

    исправлена ошибка с ди контейнерами в тг боте

--- SMMTracker.TgBot/Program.cs
+++ SMMTracker.TgBot/Program.cs
@@ -39,6 +39,9 @@ static class Program
         
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlite(connectionString));
+
+        services.AddScoped<IApplicationDbContext>(sp => 
+            sp.GetRequiredService<ApplicationDbContext>());
         
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IUserService, UserService>();
